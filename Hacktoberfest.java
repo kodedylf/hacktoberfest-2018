91a87cac82b729cfb56c9a7f46b049697464c077
Hans Kilian <hans@kilianconsult.dk>
2018-10-16 19:57:15 +0200

    Hahaha

--- Hacktoberfest.java
+++ Hacktoberfest.java
@@ -1,6 +1,7 @@
 public class Hacktoberfest {
      public static void main(String[] args) {
      	System.out.println("Hej Mathias");
-         System.out.println("Hej Hans :-)");
+        System.out.println("Hej Hans :-)");
+        System.out.println("Boros sucks!");
      }
 }
