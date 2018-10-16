5fa70e5eb05fe9a03aafe20342d822f931b1bc28
ro3d <ro3d@Mathias>
2018-10-16 20:00:08 +0200

    Added iAmThinkingOfANumber();

--- Hacktoberfest.java
+++ Hacktoberfest.java
@@ -2,5 +2,12 @@ public class Hacktoberfest {
      public static void main(String[] args) {
      	System.out.println("Hej Mathias");
          System.out.println("Hej Hans :-)");
+         iAmThinkingOfANumber();
+     }
+     
+     public static void iAmThinkingOfANumber()
+     {
+        int a = (int) (Math.random() * 10 + 1);
+        System.out.println("Jeg tænkte på: " + a);
      }
 }
